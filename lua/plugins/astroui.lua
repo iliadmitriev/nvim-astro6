b60b0819e27e511c6b692212ee80866921edbca5
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2026-03-31 19:46:35 +0300

    add link

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -10,7 +10,7 @@ return {
   "AstroNvim/astroui",
   ---@type AstroUIOpts
   opts = {
-    -- change colorscheme
+    -- change colorscheme at ./lua/plugins/catppuccin.lua
     -- colorscheme = "astrodark",
     -- -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     -- highlights = {
